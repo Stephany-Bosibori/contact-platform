e69cb6c3996f3945ccb0dbd7031de08265130e5c
Stephany-Bosibori <nyagaka.stephany@strathmore.edu>
2026-06-18 12:19:13 +0300

    20th commit

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ function initPortfolioFiltering() {
 }
 
 /**
- * INTERACTIVE FEATURE 3: Real-time Form Validation Handler
+ * INTERACTIVE FEATURE 3: Real-time Form Validation Handlers
  */
 function initFormValidation() {
     const registrationForm = document.getElementById('enrollmentForm');
